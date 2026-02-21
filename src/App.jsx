f61a15a1d956a88188f2e1e2c14fedaf2c7d9605
manoj <2500032214@kluniversity.in>
2026-02-21 12:26:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { BrowserRouter} from 'react-router-dom';
+import './App.css';
+import MainNavBar from './pages/MainNavBar';
+
+function App() {
+ 
+
+  return (
+   
+       <BrowserRouter>
+         <MainNavBar/>
+     </BrowserRouter>
+      
+  )
+}
+
+export default App
